f1ec7939abab9fe5ed4b597b0f1c4d3f9cdb8421
Luisa Ferreira <luisaaferreira23@gmail.com>
2023-04-11 18:09:08 -0300

    Implementada a navegação

--- app/src/main/java/com/example/bloconotas/ListaNotasFragment.kt
+++ app/src/main/java/com/example/bloconotas/ListaNotasFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.example.bloconotas.databinding.FragmentListaNotasBinding
 
 class ListaNotasFragment : Fragment(R.layout.fragment_lista_notas) {
     private lateinit var binding: FragmentListaNotasBinding
+    private lateinit var navigation: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,12 +21,18 @@ class ListaNotasFragment : Fragment(R.layout.fragment_lista_notas) {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        navigation = findNavController()
+
         binding = FragmentListaNotasBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.adicionarNota.setOnClickListener{
+            navigation.navigate(R.id.action_listaNotasFragment_to_adicionarNotaFragment)
+        }
+
 
 
     }
